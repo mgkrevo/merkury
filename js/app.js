7755a929f98a67b58b491b5f4b9e4572fe21cc3f
Maksym Kryvoruchko <mgkrevo@gmail.com>
2020-07-08 09:53:07 +0300

    Add Smooth Links, Complete Features Info Section, Fix Some Bugs

--- js/app.js
+++ js/app.js
@@ -1,8 +1,16 @@
 new WOW().init();
 
-window.addEventListener('scroll', () => {
-    animateHeader();
-});
+init();
+
+function init() {
+    const smoothLinks = document.querySelectorAll('a[href^="#"]');
+    
+    window.addEventListener('scroll', () => {
+        animateHeader();
+    });
+
+    smoothScroll(smoothLinks);
+}
 
 function animateHeader() {
     const currentY = pageYOffset;
@@ -17,4 +25,18 @@ function animateHeader() {
     }
 
     return;
+}
+
+function smoothScroll(smoothLinks) {
+    for (let smoothLink of smoothLinks) {
+        smoothLink.addEventListener('click', function (e) {
+            e.preventDefault();
+            const id = smoothLink.getAttribute('href');
+
+            document.querySelector(id).scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        });
+    };
 }
